bb39e5529183e69a2fe945db9da5a0cde20c4639
Matthew Gordon <laxredemption5525@gmail.com>
2017-07-14 17:31:40 -0600

    Add test skeleton, encodeToken() test

---
+++ test/unit/auth.local.test.js
@@ -0,0 +1,20 @@
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const should = chai.should();
+
+const localAuth = require('../../src/server/auth/local');
+
+describe('auth : local', () => {
+
+  describe('encodeToken()', () => {
+
+    it('should return a token', () => {
+      const results = localAuth.encodeToken({id: 1});
+      should.exist(results);
+      results.should.be.a('string');
+    });
+
+  });
+
+});
