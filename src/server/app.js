37a28b1e1c3e8f9f03845f4ccdcfe61e655582a1
Matthew Gordon <laxredemption5525@gmail.com>
2017-07-14 16:15:50 -0600

    Add error handlers, and CORS fix

--- src/server/app.js
+++ src/server/app.js
@@ -2,12 +2,57 @@
 
 const express = require('express');
 const app = express();
+const logger = require('morgan');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 
 require('dotenv').config();
 
+
+if (process.env.NODE_ENV !== 'test') {
+  app.use(logger('dev'));
+}
+
+// *** cross domain requests *** //
+const allowCrossDomain = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+};
+
+app.use(allowCrossDomain);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// error handlers
+
+// development error handlers
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handlers
+// no stacktrace leaked to user
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    error: err
+  });
+});
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 module.exports = app;
